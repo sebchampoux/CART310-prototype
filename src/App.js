9136b7fae11d2c12a7e66a8ead17db225a3239e8
Sébastien Champoux <contact@sebastienchampoux.com>
2021-04-01 17:27:43 -0400

    Added Open Street Maps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
+      <div className="app-root">
+        <MapContainer center={[40, -80]} zoom={4} scrollWheelZoom>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     )
   }
